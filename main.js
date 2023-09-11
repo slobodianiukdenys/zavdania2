b8243134fd00461c9d541f803f7aca2ff181ee7e
slobodianiukdenys <144223224+slobodianiukdenys@users.noreply.github.com>
2023-09-11 20:49:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let number = 15
+let NaN = NaN 
+let Text = "Hello"
+let TrueFalse = true
+let undefined = undefined
+
+let Me = {
+    Name: "Denys",
+    Surname: "Slobodianiuk",
+    Age: "17",
+    Town: "Tlumach"
+}
+
+console.log(number, NaN, Text, TrueFalse, undefined)
+console.table(Me)
